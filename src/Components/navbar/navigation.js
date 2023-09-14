0af6fb1e12364867545e231a54910c1d8496df55
Amirabotaref1995 <abotarefamir@gmail.com>
2023-09-14 15:30:48 +0300

    start new base project

---
+++ src/Components/navbar/navigation.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const navigation = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+          {/* <Link to="/contact">Contact</Link> */}
+        </li>
+      </ul>
+    </nav>
+  );
+};
